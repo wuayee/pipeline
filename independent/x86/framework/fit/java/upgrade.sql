937e04b3d95b181fe4b3ec5b2285d84fcce85071
wuayee <wuayee@noreply.gitcode.com>
2025-10-16 16:34:25 +0800

    [app-builder] 升级sql，用于内置应用的隐藏，和资源组字段的优化

--- independent/x86/framework/fit/java/upgrade.sql
+++ independent/x86/framework/fit/java/upgrade.sql
@@ -1,92 +1,18 @@
--- 在store_tool与store_definition中添加order字段
-UPDATE "public"."store_tool"
-SET "schema" = jsonb_set(
-    "schema"::jsonb,
-    '{order}',
-    ("schema"::jsonb->'parameters'->'required')::jsonb
-)
-WHERE "unique_name" IN (
-    '25887d76-e358-4121-800c-31eb3390fdbd',
-    '3bca6a3f-9623-4228-b120-1a5e0d41dc14',
-    'e1d24716-9ffa-46e5-bd36-d3a8d8142559',
-    'eeedf03c-10aa-41f1-b5e3-6623914242c8',
-    'e2bd847f-2cfa-4cdc-b326-4d15a15c6a0d'
-)
-AND ("schema"->>'order') IS NULL;
+UPDATE app_builder_app
+SET create_by = 'system',
+    update_by = 'system'
+WHERE app_suite_id IN ('ec6f8e93a80541bb930fc22678ef7043',
+                       '5185dad4c8124522a2612c20f8497cf0',
+                       '0b4fe5a430104edfbe0dc6cff0ebea19',
+                       'dfe319109bc84f6793645e3483c029ca');
 
-UPDATE "public"."store_definition"
-SET "schema" = jsonb_set(
-    "schema"::jsonb,
-    '{order}',
-    ("schema"::jsonb->'parameters'->'required')::jsonb
-)
-WHERE "name" IN (
-     'knowledge-retrieval-def-name',
-     'application-text-extraction-def-name',
-     'document-extractor-def-name',
-     'image-generation-def-name',
-     'document-extraction-def-name'
-)
-AND ("schema"->>'order') IS NULL;
-
--- 更新 store_tool 表
-UPDATE "public"."store_tool"
-SET "schema" = (
-    WITH transformed AS (
-        SELECT
-            jsonb_set(
-                "schema"::jsonb,
-                '{parameters,properties}',
-                (
-                    SELECT jsonb_object_agg(
-                        key,
-                        CASE
-                            -- 保留名为 examples 的参数
-                            WHEN key = 'examples' THEN value
-                            -- 其他参数移除 examples 和布尔类型的 required
-                            ELSE
-                                value::jsonb
-                                - 'examples'
-                                - CASE WHEN jsonb_typeof(value->'required') = 'boolean'
-                                      THEN 'required'
-                                      ELSE ''
-                                 END
-                        END
-                    )
-                    FROM jsonb_each(("schema"::jsonb)->'parameters'->'properties')
-                )
-            ) AS new_schema
-    )
-    SELECT new_schema FROM transformed
-)
-WHERE ("schema"::jsonb)->'parameters'->'properties' IS NOT NULL;
-
--- 同样的更新应用于 store_definition 表
-UPDATE "public"."store_definition"
-SET "schema" = (
-    WITH transformed AS (
-        SELECT
-            jsonb_set(
-                "schema"::jsonb,
-                '{parameters,properties}',
-                (
-                    SELECT jsonb_object_agg(
-                        key,
-                        CASE
-                            WHEN key = 'examples' THEN value
-                            ELSE
-                                value::jsonb
-                                - 'examples'
-                                - CASE WHEN jsonb_typeof(value->'required') = 'boolean'
-                                      THEN 'required'
-                                      ELSE ''
-                                 END
-                        END
-                    )
-                    FROM jsonb_each(("schema"::jsonb)->'parameters'->'properties')
-                )
-            ) AS new_schema
-    )
-    SELECT new_schema FROM transformed
-)
-WHERE ("schema"::jsonb)->'parameters'->'properties' IS NOT NULL;
+ALTER TABLE app_builder_app
+    ALTER COLUMN user_group_id DROP NOT NULL;
+ALTER TABLE app_builder_form
+    ALTER COLUMN user_group_id DROP NOT NULL;
+ALTER TABLE store_plugin
+    ALTER COLUMN user_group_id DROP NOT NULL;
+ALTER TABLE store_plugin_tool
+    ALTER COLUMN user_group_id DROP NOT NULL;
+ALTER TABLE store_app
+    ALTER COLUMN user_group_id DROP NOT NULL;
